7cd22a308ab3205025d05bbee970c6c702701f0b
KRUSHAL PRAJAPATI <krushalprajapati@KRUSHALs-MacBook-Pro.local>
2026-07-12 23:45:31 +1000

    Completed SIT725 1.1P practical

---
+++ 1.1P/source files/app.js
@@ -0,0 +1,23 @@
+console.log("Welcome to SIT725");
+
+console.log("Introduction to Programming");
+
+let studentName = "Krushal Prajapati";
+
+let studentID = "s225789454";
+
+console.log(studentName);
+
+console.log(studentID);
+
+let a = 15;
+
+let b = 25;
+
+console.log("Addition =", a + b);
+
+console.log("Subtraction =", b - a);
+
+console.log("Multiplication =", a * b);
+
+console.log("Division =", b / a);
